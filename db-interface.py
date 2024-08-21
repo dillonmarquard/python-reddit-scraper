6ebf022cd45509493cb6eda1e592e24a5cbc9ee5
Dillon Marquard <dmarquard4@gmail.com>
2024-08-20 19:29:27 -0700

    init

---
+++ db-interface.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class SqLiteDB:
+    """A sqlite3 interface to save subreddit post and comment data"""
+    
+    def __init__(self):
+        self._con = sqlite3.connect("reddit-of02j.db")
+        self.cur = con.cursor()
+
+    def insert_update_post() -> None:
+        """update + insert data about a post on a subreddit"""
+        pass
+
+    def insert_update_comment() -> None:
+        """update + insert data about a comment for a post"""
+        pass
---
+++ reddit-interface.py
@@ -0,0 +1,9 @@
+
+
+class RedditAPI:
+    """Reddit API wrapper for pulling subreddit and (sub-)comment data"""
+
+    def __init__():
+        
+        with open("secrets.cfg") as f:
+            self._secret = f.readline()
